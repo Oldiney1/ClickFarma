c780ec72ffad6c952940521d67ef44e435708dd4
Oldiney Galvão <oldiney15@gmail.com>
2025-04-02 18:47:33 -0300

    feat: adição de navbar funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', component: HomeComponent},
+{path: 'login', component: LoginComponent},
+{path: 'cadastrar', component: CadastrarComponent},
+{path: 'carrinho', component: CarrinhoComponent}
+];
+    
---
+++ src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-cadastrar',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './cadastrar.component.html',
+  styleUrl: './cadastrar.component.scss'
+})
+export class CadastrarComponent {
+
+}
